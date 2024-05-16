4458e8bd79ad3cd2d02c8ef0033754d63278c733
Sanniv Choudhuri <sanniv.1020@gmail.com>
2024-05-16 16:15:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,3 +19,55 @@ const getOptions = (querySelectorTag) => {
     }
     return { optionValues, optionTexts }
 }
+
+async function delay(time){
+    return new Promise((res) => setTimeout(res, time))
+}
+
+function checkbox(selector) {
+    const element = document.querySelector(selector);
+    if (element && element.type === 'checkbox') {
+        element.click();
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function clickButton(selector) {
+    const button = document.querySelector(selector);
+    if (button && button.tagName === 'BUTTON') {
+        button.click();
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function selectDropdownOption(selector, optionValue) {
+    const dropdown = document.querySelector(selector);
+    dropdown.value = optionValue;
+    dropdown.dispatchEvent(new Event("input", { bubbles: true }));
+    dropdown.dispatchEvent(new Event("change", { bubbles: true }));
+}
+
+function fillInputBox(selector, inputValue){
+    const element = document.querySelector(selector)
+    element.value = inputValue
+    element.dispatchEvent(new Event("input", { bubbles: true }));
+    element.dispatchEvent(new Event("change", { bubbles: true }));
+}
+
+function selectDropdownOptionByInnerHTML(selector, innerHTML) {
+    const dropdown = document.querySelector(selector);
+    const options = dropdown.querySelectorAll('option');
+    options.forEach(option => {
+        if (option.innerHTML.trim() === innerHTML.trim()) {
+            option.selected = true;
+        } else {
+            option.selected = false;
+        }
+    });
+    dropdown.dispatchEvent(new Event("input", { bubbles: true }));
+    dropdown.dispatchEvent(new Event("change", { bubbles: true }));
+}
